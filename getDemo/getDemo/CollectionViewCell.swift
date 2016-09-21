a7a32d442bfe1354aca6169ad647184d4bb407a2
qianfeng <qianfeng@kimch.local>
2016-09-21 10:34:36 +0800

    新建文件和修改文件 勾选必要的选项

---
+++ getDemo/getDemo/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  getDemo
+//
+//  Created by qianfeng on 16/9/21.
+//  Copyright © 2016年 kimCheng. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
