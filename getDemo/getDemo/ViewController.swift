4f3daef7cb3659b46bdf04449bc7e2f6431ddbac
qianfeng <qianfeng@kimch.local>
2016-09-21 10:12:57 +0800

    初始化上传

---
+++ getDemo/getDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getDemo
+//
+//  Created by qianfeng on 16/9/21.
+//  Copyright © 2016年 kimCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
